299ef9bdff5f5646305b77825263feac7a9ca493
LangaLee <langa4749@gmail.com>
2024-08-04 13:43:08 +0000

    creates number guessing game

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿Random dice = new();
+
+int secretValue = dice.Next(0,101);
+
+int guess = 0;
+string? input;
+bool numInput = false;
+Console.WriteLine("Welcome to the guessing game!!!!");
+Console.WriteLine("Please guess a number between 0 and 101 \n");
+
+do {
+    input = Console.ReadLine();
+    numInput = int.TryParse(input, out guess);
+    string? result;
+    if (guess < secretValue) result = "less than the number";
+    else if (guess > secretValue) result = "bigger than the number";
+    else result = "You win!!!!!!!!!";
+ 
+    if (numInput) {
+        if (result != "You win!!!!!!!!!") Console.WriteLine($"You guessed {input} which is {result}!\n");
+
+        if (result == "You win!!!!!!!!!") {
+            Console.WriteLine(result);
+            break;
+        }
+
+    } else {
+        Console.WriteLine("Please input a number");
+    }
+
+} while (numInput);
